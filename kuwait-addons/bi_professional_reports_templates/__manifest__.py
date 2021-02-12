69a3153f76b2380782ab884abf57f5b97f4dba78
Your Name <you@example.com>
2021-02-13 00:32:41 +0200

    [FIX]222232

--- kuwait-addons/bi_professional_reports_templates/__manifest__.py
+++ kuwait-addons/bi_professional_reports_templates/__manifest__.py
@@ -14,7 +14,7 @@
     'author': 'BrowseInfo',
     'live_test_url':'https://youtu.be/_aihFWW4a5E',
     'website': 'https://www.browseinfo.in',
-    'depends': ['base', 'account', 'sale', 'purchase', 'stock', 'sale_stock', 'base_vat','sale_management','purchase_stock'],
+    'depends': ['base', 'account', 'sale', 'purchase', 'stock', 'sale_stock', 'base_vat','sale_management','purchase_stock','open_purchase'],
     'data': [
 
         "res_company.xml",
