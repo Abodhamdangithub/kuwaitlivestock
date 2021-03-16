3fe2a37ab14ef8a950a8ec227dec38817b7cf37e
Your Name <you@example.com>
2021-03-16 20:44:02 +0200

    [Push Reports]2222

--- kuwait-addons/open_purchase/reports_kuwait/reports_kuwait.py
+++ kuwait-addons/open_purchase/reports_kuwait/reports_kuwait.py
@@ -11,7 +11,7 @@ class ReportsKuwait(models.Model):
     from_date = fields.Date(string="From Date")
     to_date = fields.Date(string="To Date")
 
-
+    product_ids = fields.Many2many('product.product',string="المنتج")
 
 
     def get_stock_quant_data(self):
@@ -28,41 +28,62 @@ class ReportsKuwait(models.Model):
 
     def get_sales_by_product_data(self):
         product_send = []
-        product_ids = self.env['product.product'].search([])
+        if self.product_ids:
+            products = self.product_ids.ids
+
+            product_ids = self.env['product.product'].search([('id','in',products)])
+        else:
+            product_ids = self.env['product.product'].search([])
         for product in product_ids:
             select = "select  COALESCE(sum(product_uom_qty),0) as product_uom_qty , COALESCE(sum(price_subtotal),0) as price_subtotal from sale_order_line  as line where product_id = %s and (select date_order from sale_order where id = line.order_id) >= %r and (select date_order from sale_order where id = line.order_id) <= %r and (select state from sale_order where id = line.order_id) <> 'cancel'"% (product.id,str(self.from_date),str(self.to_date))
             self.env.cr.execute(select)
             result = self.env.cr.dictfetchall()
+            print ("result",result)
             if result[0]['product_uom_qty'] > 0:
                 product_send.append([product.name,result[0]['product_uom_qty'],result[0]['price_subtotal']])
         return product_send
 
     def get_sales_by_lines_data(self):
         product_send = []
-        product_ids = self.env['product.product'].search([])
+        if self.product_ids:
+            products = self.product_ids.ids
+
+            product_ids = self.env['product.product'].search([('id','in',products)])
+        else:
+            product_ids = self.env['product.product'].search([])
+
         for product in product_ids:
             select = "select  (select name from sale_order where id = line.order_id) as order_name,COALESCE(product_uom_qty,0) as product_uom_qty ,COALESCE(price_unit,0) as price_unit , COALESCE(price_subtotal,0) as price_subtotal from sale_order_line  as line where product_id = %s and (select date_order from sale_order where id = line.order_id) >= %r and (select date_order from sale_order where id = line.order_id) <= %r and (select state from sale_order where id = line.order_id) <> 'cancel'"% (product.id,str(self.from_date),str(self.to_date))
             self.env.cr.execute(select)
             result = self.env.cr.dictfetchall()
+            print ("result",result)
             if len(result) > 0:
-                product_send.append([product.name,result[0]['order_name'],result[0]['product_uom_qty'],result[0]['price_unit'],result[0]['price_subtotal']])
+                for r in result:
+                    product_send.append([product.name,r['order_name'],r['product_uom_qty'],r['price_unit'],r['price_subtotal']])
         return product_send
 
 
     def get_partner_balance_data(self):
         partner_send = []
         partner_ids = self.env['res.partner'].search([('customer_rank','>',0)])
+
+        sum = 0.0
         for partner in partner_ids:
             print (partner.customer_rank)
             if partner.balance_partner > 0 or partner.balance_partner < 0:
                 partner_send.append([partner.name,partner.balance_partner])
+                sum += partner.balance_partner
+        partner_send.append(['المجموع', sum])
         return partner_send
 
 
     def get_supplier_balance_data(self):
         partner_send = []
         partner_ids = self.env['res.partner'].search([('supplier_rank','>',0)])
+        sum = 0.0
         for partner in partner_ids:
             if partner.balance_partner > 0 or partner.balance_partner < 0:
                 partner_send.append([partner.name,partner.balance_partner])
+                sum += partner.balance_partner
+        partner_send.append(['المجموع', sum])
         return partner_send
